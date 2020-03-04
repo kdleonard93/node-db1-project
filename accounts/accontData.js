312bec0a603bcf462e61ca9c09ebe2bccd5fa4a5
Kyle Leonard <kdleonard@icloud.com>
2020-03-03 18:18:03 -0600

    GET call

--- accounts/accontData.js
+++ accounts/accontData.js
@@ -4,7 +4,15 @@ const db = require("../data/dbConfig");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  db.select("accounts")
-    .from("accounts")
-    .then(accounts => {});
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Could not retrieve the list of accounts"
+      });
+    });
+
+  router.get("/", (req, res) => {});
 });
