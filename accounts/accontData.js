dacb6e4c85dd4e5f994462f85e22f8f44b0e15bd
Kyle Leonard <kdleonard@icloud.com>
2020-03-03 18:22:50 -0600

    GET all by ID

--- accounts/accontData.js
+++ accounts/accontData.js
@@ -14,5 +14,18 @@ router.get("/", (req, res) => {
       });
     });
 
-  router.get("/", (req, res) => {});
+  router.get("/:id", (req, res) => {
+    db("accounts")
+      .where({ id: req.params.id })
+      .first()
+      .then(account => {
+        if (account) {
+          res.status(200).json(account);
+        } else {
+          res.status(404).json({
+            message: "Account can not be found"
+          });
+        }
+      });
+  });
 });
